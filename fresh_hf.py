95e507904fbceb75008852c5fe913c66ce29c5c7
noengineeringdegree <jon.logrippo@gmail.com>
2025-08-06 20:58:48 -0400

    stop merging

--- fresh_hf.py
+++ fresh_hf.py
@@ -2,9 +2,8 @@ import os
 import sys
 from litellm import completion
 from dotenv import load_dotenv
-from smolagents import CodeAgent, OpenAIServerModel, Tool
-from package.subpackage.module import toolbox
-
+from smolagents import CodeAgent, OpenAIServerModel
+from toolbox.work_commute_tool import Get_Work_Commute
 
 approved_sources = ["zillow", "apartments.com", "homes.com rent"]
 neighboorhoods = ["allston", "brighton", "brookline", "quincy"]
@@ -20,7 +19,7 @@ if not openai_api_key:
     raise ValueError("ur silly")
 
 model = OpenAIServerModel(model_id="gpt-4o-mini",api_key=openai_api_key)
-agent = CodeAgent(tools=[],model=model)
+agent = CodeAgent(tools=[Get_Work_Commute()],model=model)
 
 try:
     agent.run(f"Look for 20 apartments in {', '.join(neighboorhoods)} for {rent} or less")
@@ -31,8 +30,6 @@ except KeyboardInterrupt:
 #Filter Apartments
 def filterApartments(work=work_address,  wont_take=banned_transport):
     pass  # TODO: Implement filtering logic
-
-# Use that decorator they have. Make as a seperate tools subpackage and struggle through the python import system like we all had to
 
 
 
